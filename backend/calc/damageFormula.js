bf7ac76ef6619ae626d7baff67fbf051ba18af69
liamsuperville <liam22@vt.edu>
2026-03-24 11:58:55 -0400

    Update to damage formula w rain sun weather and added typechart

--- backend/calc/damageFormula.js
+++ backend/calc/damageFormula.js
@@ -1,18 +1,30 @@
+const { getTypeEffectiveness } = require('./typeChart');
+const { getWeatherModifier } = require('./weatherModifiers');
 function calculateDamage(attacker, defender, move, conditions) 
 {
+    // If not physical, then special
+    const physicalTypes = ['Normal', 'Fighting', 'Flying', 'Poison', 'Ground', 'Rock', 'Bug', 'Ghost', 'Steel'];
+    const isPhysical = physicalTypes.includes(move.type);
+
+    const atk = isPhysical ? attacker.attacl : attacker.sp_attack;
+    const def = isPhysical ? defender.defense : defender.sp_defense;
+
+    // base damage formula
+    const baseDamage = Math.floor(((2 * attacker.level / 5 + 2) * move.basePower * atk / def) / 50 + 2);
+
+    // stab mod
+    const stab = (attacker.type1 === move.type || attacker.type2 === move.type) ? 1.5 :1;
+
+    // type mod
+    const typeEffectiveness = getTypeEffectiveness(move.type.toLowerCase(), defender.type1.toLowerCase(), defender.type2 ? defender.type2.toLowerCase() : null);
+    
+    // weather mod
+    const weather = getWeatherModifier(move.type, conditions.weather);
+
+    const crit = conditions.isCrit ? 2 : 1;
+
+    const burn = (conditions.isBurned && isPhysical) ? 0.5 : 1;
 
 }
 
 module.exports = { calculateDamage };
-
-// If not physical, then special
-const physicalTypes = ['Normal', 'Fighting', 'Flying', 'Poison', 'Ground', 'Rock', 'Bug', 'Ghost', 'Steel'];
-const isPhysical = physicalTypes.includes(move.type);
-
-const atk = isPhysical ? attacker.attacl : attacker.sp_attack;
-const def = isPhysical ? defender.defense : defender.sp_defense;
-
-// Base damage formula
-const baseDamage = Math.floor(((2 * attacker.level / 5 + 2) * move.basePower * atk / def) / 50 + 2);
-
-const stab = (attacker.type1 === move.type || attacker.type2 === move.type) ? 1.5 :1;
