a456c596421ed39589f5ff1fe1de8f36f8b1c980
z <elamjay@vt.edu>
2026-05-04 22:52:00 -0400

    testing port numbers

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,2 @@
 const app = require("./server");
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(5000, () => console.log("Server running on port 5000"));
