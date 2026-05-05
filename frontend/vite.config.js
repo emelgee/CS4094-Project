bf9f7fa36028b956c4ec3112b60ab85c3c1b7b69
z <elamjay@vt.edu>
2026-05-04 23:22:21 -0400

    all hosts changed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: 'all',
+    allowedHosts: true,
     port: parseInt(process.env.PORT) || 3000,
     host: '0.0.0.0'
   }
